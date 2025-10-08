4c306d747cecc1430d192f64baa314ef5a7279ac
apply <wonji052271@gmail.com>
2025-10-09 00:12:21 +0900

    B5_2742

---
+++ BOJ/Cpp/B5_2742/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // input
+    int n;
+    cin >> n;
+
+    // logic & output
+    for (int i = n; i >= 1; i--){
+        cout << i << "\n";
+    }
+
+    
+    // END
+    return 0;
+}
