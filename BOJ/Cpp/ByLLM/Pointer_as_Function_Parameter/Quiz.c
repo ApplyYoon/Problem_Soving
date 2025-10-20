bf2ba8f6535bae9256b8c64efb3f08e43942f4e5
apply <wonji052271@gmail.com>
2025-10-21 01:57:23 +0900

    Pointer as function parameter

---
+++ BOJ/Cpp/ByLLM/Pointer_as_Function_Parameter/Quiz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    // TODO: 포인터를 이용한 값 교환
+}
+
+int main(void) {
+    int a = 3, b = 7;
+    swap(&a, &b);
+    printf("%d %d\n", a, b); // 출력: 7 3
+    return 0;
+}
