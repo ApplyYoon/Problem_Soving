6251aedc913b7a1f9a934c27d7a6e4b52bd17398
apply <wonji052271@gmail.com>
2025-10-21 02:01:27 +0900

    Double Pointer as function parameter

---
+++ BOJ/Cpp/ByLLM/Double_Pointer_as_FP/solution.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void change_pointer(int **pp) {
+    // TODO: 이중 포인터를 이용해서
+    //       포인터가 b를 가리키게 만드세요.
+    static int b = 20;
+    *pp = &b;
+
+    return;
+}
+
+int main(void) {
+    int a = 10, b = 20;
+    int *p = &a;
+
+    printf("Before: *p = %d\n", *p);
+
+    change_pointer(&p);  // p의 주소 전달
+
+    printf("After: *p = %d\n", *p);  // 출력: 20
+    return 0;
+}
